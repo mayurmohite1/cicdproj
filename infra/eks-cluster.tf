b924df5b29550c432a50a188ad1ad0fe1d942790
mayurmohite1 <mayurmohite1562@gmail.com>
2026-01-12 11:18:06 +0000

    Add Terraform provider binaries and update EKS cluster configuration
    - Added null provider binary version 3.2.4.
    - Added time provider binary version 0.13.1 and its LICENSE file.
    - Added tls provider binary version 4.1.0 and its LICENSE file.
    - Updated EKS cluster configuration in eks-cluster.tf:
      - Enabled cluster creator admin permissions.
      - Adjusted desired node size from 3 to 2 to avoid capacity issues.
      - Added additional IAM role policies for EKS worker nodes.

--- infra/eks-cluster.tf
+++ infra/eks-cluster.tf
@@ -2,13 +2,16 @@ module "eks" {
   source  = "terraform-aws-modules/eks/aws"
   version = "21.6.1"
 
-  name                   = "myapp-eks-cluster"
-  kubernetes_version     = "1.31"
-  endpoint_public_access = true
+  name                                     = "myapp-eks-cluster"
+  kubernetes_version                       = "1.31"
+  endpoint_public_access                   = true
+  enable_cluster_creator_admin_permissions = true
+
 
   # Configure the VPC and Subnets -->
   subnet_ids = module.myapp-vpc.private_subnets
   vpc_id     = module.myapp-vpc.vpc_id
+
 
   addons = {
     vpc-cni    = { most_recent = true }
@@ -21,9 +24,14 @@ module "eks" {
     dev = {
       min_size     = 1
       max_size     = 3
-      desired_size = 3 # Start with 1 node to avoid capacity issues
+      desired_size = 2 # Start with 1 node to avoid capacity issues
 
       instance_types = ["t3.medium"] # Use t3.small instead of t3.micro for more resources
+      iam_role_additional_policies = {
+        AmazonEKSWorkerNodePolicy          = "arn:aws:iam::aws:policy/AmazonEKSWorkerNodePolicy"
+        AmazonEKS_CNI_Policy               = "arn:aws:iam::aws:policy/AmazonEKS_CNI_Policy"
+        AmazonEC2ContainerRegistryReadOnly = "arn:aws:iam::aws:policy/AmazonEC2ContainerRegistryReadOnly"
+      }
     }
   }
   # The tags are optional -->
